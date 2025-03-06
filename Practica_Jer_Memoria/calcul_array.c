512c88a957798c595cc4daf47175f38192ba5648
pllinasv <pol.llinas06@gmail.com>
2025-03-06 12:11:48 +0100

    feat(calcul_array.c): Added QuickSort, sorting from highest to lowest value

--- Practica_Jer_Memoria/calcul_array.c
+++ Practica_Jer_Memoria/calcul_array.c
@@ -3,6 +3,47 @@
 
 #define MAX_SIZE 100
 
+void printArray(int numbers[]){
+    for(int i=0; i<MAX_SIZE; i++){
+        printf("%d\n", numbers[i]);
+    }
+}
+
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int array[], int low, int high){
+    int pivot=array[high];
+    int i = (low -1);
+
+    for(int j = low; j<high; j++){
+        if(array[j] > pivot){
+            i++;
+            swap(&array[i], &array[j]);
+        }
+    }
+    swap(&array[i+1], &array[high]);
+    return(i+1);
+}
+
+void quickSort(int array[], int low, int high){
+    if( low<high ){
+        int pi = partition(array, low, high);
+
+        quickSort(array, low, pi-1);
+        quickSort(array, pi+1, high);
+    }
+}
+
+void copyArray(int from[], int to[]){
+    for(int i=0; i<MAX_SIZE; i++){
+        to[i]=from[i];
+    }
+}
+
 int main() {
     FILE *values;
     char fileName[] = "valors.txt";
@@ -24,9 +65,13 @@ int main() {
     
     fclose(values);
     
-    for(int i=0; i<MAX_SIZE; i++){
-        printf("%d\n", numbers[i]);
-    }
+    int orderedArray[MAX_SIZE];
+
+    copyArray(numbers, orderedArray);
+
+    quickSort(orderedArray, 0, MAX_SIZE-1);
+
+    printArray(orderedArray);
 
     return 0;
 }
